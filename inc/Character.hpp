358e0f1dfee712db49cfc2592b7757bc32faf7b8
YiYhuan <yhuancr@gmail.com>
2025-05-07 00:12:51 +0000

    draw the player and move wasd

--- inc/Character.hpp
+++ inc/Character.hpp
@@ -1,14 +1,23 @@
 #pragma once
 
 #include <SDL.h>
+#include <string>
 
 // TODO: implement this class
 class Character {
   public:
     SDL_Texture* sprite;
+    //SDL_Texture* texture;
+    SDL_Rect rect;
+    int step;
 
     Character();
+
+    Character(SDL_Renderer* renderer, const std::string& imagePath, int startX, int startY);
     ~Character();
+
+    void draw(SDL_Renderer* renderer);
+    void handleInput(const SDL_Event& e);
 
     bool loadSprite(SDL_Renderer* renderer, const char* spritePath);
 };
